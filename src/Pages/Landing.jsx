cf33340f9097d1b21de6941c1880edbe1c683457
Samuel Ortiz <samuelortiz@MacBook-Pro-de-Samuel.local>
2023-10-11 11:22:18 -0500

    landing final

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -7,12 +7,17 @@ export function Landing () {
         <div>
                 <Nav/>
 
-            <section className="flex flex-col items-center text-center gap-9 px-10 bg-landing-bg bg-cover py-36">
-                <h1 className="font-bold text-8xl text-white w-6/12">El dulce sabor <span className="text-yellow">Tradicional</span></h1>
-                <p className="text-white w-5/12">Sabores auténticos del Valle del Cauca. Deléitate con nuestros dulces típicos artesanales, que capturan la esencia y tradición de esta hermosa región. Descubre la dulzura que nos distingue.</p>
-                <Link to="/about">
-                    <Button color="red" size="large" className="rounded-full" ripple>conocenos</Button>
-                </Link>
+            <section className="flex px-10 bg-landing-bg bg-cover py-36">
+                <div className="flex flex-col gap-9">
+                    <h1 className="font-bold text-8xl text-white">El dulce sabor <span className="text-yellow">Tradicional</span></h1>
+                    <h5 className="text-white w-9/12">Sabores auténticos del Valle del Cauca. Deléitate con nuestros dulces típicos artesanales, que capturan la esencia y tradición de esta hermosa región. Descubre la dulzura que nos distingue.</h5>
+                    <Link to="/about">
+                        <Button color="green" size="lg" ripple>conocenos</Button>
+                    </Link>
+                </div>
+                <div className="flex pr-16 items-center justify-center">
+                    <img className="w-min" src="./src/assets/logo-grande.svg" alt="" />
+                </div>
             </section>
         </div>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,16 +4,16 @@ export function Nav () {
 
     const isActive = ({isActive}) => {
         return isActive
-        ? 'text-redish border-b border-solid border-redish'
-        : 'hover:text-yellow'
+        ? 'text-redish border-b border-redish border-solid text-redish'
+        : 'text-green-dark rounded-xl hover:text-white'
     }
 
     return (
-        <nav className="flex justify-between items-center w-full px-10 py-3 bg-green-light text-green-dark">
+        <nav className="flex justify-between items-center w-full px-10 py-3 bg-yellow">
             <NavLink to='/'>
                 <img className="w-20 hover:scale-110 cursor-pointer transition-all duration-200" src="./src/assets/vallegrande-logo.svg" alt="logo de la empresa" />
             </NavLink>
-            <div className="flex items-center gap-6">
+            <div className="flex items-center gap-6 text-sm">
                 <NavLink to='/products' className={isActive}>Productos</NavLink>
                 <NavLink to='/about' className={isActive}>Acerca de nosotros</NavLink>
                 <NavLink to='/contact' className={isActive}>Contáctanos</NavLink>

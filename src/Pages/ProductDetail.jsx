3f0429cffa706abc4eb2f212ae549ba93df0d1bb
Samuel Ortiz <samuelortiz@MacBook-Pro-de-Samuel.local>
2023-10-12 11:15:26 -0500

    design detail product

--- src/Pages/ProductDetail.jsx
+++ src/Pages/ProductDetail.jsx
@@ -2,18 +2,21 @@ import { useParams } from "react-router-dom"
 import { Nav } from "../components/Nav"
 import { Link } from "react-router-dom"
 import { Button } from "@material-tailwind/react"
+import { productos } from "../mocks/productos"
 
 export function ProductDetail () {
 
     const {id} = useParams()
-
+    const productDetail = productos.find(product => product.id === id)
+    const {name} = productDetail
+    
     return (
         <div>
             <Nav/>
             <section className="px-20 py-10">
                 <Link to="/products">
                     <Button size="sm" className="flex items-center gap-3" variant="outlined" color="red">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-left" width="28" height="28" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#C5352C" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-left" width="28" height="28" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#E25241" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M5 12l14 0" />
                         <path d="M5 12l6 6" />
@@ -24,8 +27,14 @@ export function ProductDetail () {
                 </Link>
             </section>
 
-            <section className="px-20 py-10">
-                <h1>{id}</h1>
+            <section className="px-20 py-10 flex justify-start gap-20">
+                <div className="grid place-items-center border border-solid border-green-dark rounded-xl p-16">
+                    <img src="/src/assets/vallegrande-logo.svg" alt="" />
+                </div>
+                <div>
+                    <h1 className="font-bold text-3xl">{name}</h1>
+                    <p>Descripcion del {name}</p>
+                </div>
             </section>
         </div>
     )
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -3,11 +3,11 @@ import { productos } from "../mocks/productos"
 
 export function ProductsList () {
     return (
-        <ul className="flex flex-wrap gap-9">
+        <ul className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-14">
             {
                 productos.map(producto => (
-                    <Link to={`/products/${producto.id}`} key={producto.id}>
-                        <li className="w-56 flex flex-col justify-center items-center gap-4 px-6 py-3 rounded-lg shadow-md">
+                    <Link to={`./${producto.id}`} key={producto.id}>
+                        <li className="w-56 flex flex-col justify-center items-center gap-4 px-6 py-3 rounded-lg shadow-md transition-all duration-200 hover:shadow-xl">
                                 <img className="w-20" src="./src/assets/vallegrande-logo.svg" alt="" />
                                 <h3>{producto.name}</h3>
                         </li>
